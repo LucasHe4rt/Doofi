935e56c74a0753e9c7e64030a4bf3e02ffbc1315
Marcus Vinicius <marcusvyny92@gmail.com>
2018-11-10 22:59:26 -0200

    adicionei o card de restaurantes

--- resources/views/restaurantes.blade.php
+++ resources/views/restaurantes.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 @section('content')
-
+    <div class="container" style="margin-top: 20px;">
+    <h1>Restaurantes</h1>
+        <div class="media col-md-6">
+            <img class="d-flex align-self-start" src="https://via.placeholder.com/513x350.png?text=Restaurante+Teste" alt="Generic placeholder image">
+            <div class="media-body pl-3">
+                <div class="nome-restaurante">
+                    Nome restaurante <small>Tempo de preparao</small>
+                </div>
+                <div class="stats">
+                    <span><i class="fa fa-phone"></i>(xx) xxxx-xxxx</span>
+                </div>
+                <div class="endereco"> Endereço </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
